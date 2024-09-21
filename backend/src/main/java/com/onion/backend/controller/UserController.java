4c91041b91c5c37b39dc8cfa595da568167473fc
sweet.dh <dlsrksrhk@gmail.com>
2024-09-21 22:49:26 +0900

    스웨거 적용 및 회원가입 탈퇴 구현

---
+++ backend/src/main/java/com/onion/backend/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.onion.backend.controller;
+
+import com.onion.backend.entity.User;
+import com.onion.backend.service.UserService;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<User> createUser(@RequestParam String username, @RequestParam String password, @RequestParam String email) {
+        User user = userService.createUser(username, password, email);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(
+            @Parameter(description = "ID of the user to be deleted", required = true) @PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.noContent().build();
+    }
+}
